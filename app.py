0d49bde71087b52dd662e27cd082d769d59f8f88
Freyta <freyta@gmail.com>
2019-06-18 06:06:53 +1000

    Revert "Merge branch 'master' of https://github.com/freyta/7Eleven-Python"
    This reverts commit 8f990825c8cb48eeb780ba60e1c064d66207bccf, reversing
    changes made to c4e00624ca16cc78c58ce4d6a61c55ba6324de37.

--- app.py
+++ app.py
@@ -456,5 +456,5 @@ if __name__ == '__main__':
     scheduler.add_job(autolocker.start_lockin, 'interval', seconds=1800)
     scheduler.start()
 
-    app.secret_key = os.urandom(12)
-    app.run(host='0.0.0.0')
+    app.secret_key = "Z"
+    app.run(host='0.0.0.0',debug=True)
